83977aa582c208b91248ff412a077b5f0dba3534
Carlos Alexandre <101848698+Kiwiabacaxi@users.noreply.github.com>
2023-03-13 14:32:17 -0300

    Update arvore.c

--- Arvore AVL/arvore.c
+++ Arvore AVL/arvore.c
@@ -61,24 +61,51 @@ void recalcula_fb(Noavl* candidato, int info){
     }
 }
 
+// Funcao para calcular a altura de um no
+int altura(Noavl *candidato) {
+    if (candidato == NULL) {
+        return 0;
+    }
+    return candidato->fb;
+}
+
+// Funcao para recalcular o "altura" de um no
+void update_fb(Noavl *candidato) {
+    int fb_esq= altura(candidato->esq);
+    int fb_dir = altura(candidato->dir);
+    if (fb_esq > fb_dir) {       // se a altura do filho a esquerda for maior que a altura do filho a direita
+        candidato->fb = 1 + fb_esq;     // altura do no = 1 + altura do filho a esquerda
+    } else {
+        candidato->fb = 1 + fb_dir;    // altura do no = 1 + altura do filho a direita
+    }
+}
+
 // funcao para fazer a rotacao simples a direita
 Noavl* rotacao_simples_dir(Noavl* tree){
     Noavl* aux = tree->esq; 
     tree->esq = aux->dir;
     aux->dir = tree;
 
+    // recalcular o fator de balanceamento primeiro recalcular o fator de balanceamento do filho a direita do no que vai ser rotacionado
+    update_fb(tree);
+    update_fb(aux);
+
     return aux;
 }
 
+/* 
 Noavl* rotacao_simples_dir2(Noavl* jorgim){
     Noavl* aux = jorgim->esq; // aux recebe o filho a esquerda do no que vai ser rotacionado
     Noavl* aux2 = aux->dir;
     jorgim->esq = aux2;
     aux->dir = jorgim;
 
+    // recalcular o fator de balanceamento primeiro recalcular o fator de balanceamento do filho a direita do no que vai ser rotacionado
+    update_fb(jorgim);
+    update_fb(aux);
 
     return aux;
-}
+} */
 
 
 // funcao para fazer a rotacao simples a esquerda
@@ -87,9 +114,76 @@ Noavl* rotacao_simples_esq(Noavl* tree){
     tree->dir = aux->esq;   // o filho da direta do no que vai ser rotacionado recebe o filho da esquerda do aux
     aux->esq = tree;        // o aux da esquerda recebe a raiz
 
+    // recalcular o fatro de balanceamento primeiro, recalcular o fator de balanceamento do filho a esquerda do no que vai ser rotacionado
+    update_fb(tree);
+    update_fb(aux);
+
     return aux;
 }
 
+// funcao para fazer a rotacao esquerda-direita
+Noavl* rotacao_esq_dir(Noavl* tree){
+    tree->esq = rotacao_simples_esq(tree->esq);
+    return rotacao_simples_dir(tree);
+}
+
+// funcao para fazer a rotacao esquerda-direita sem usar a funcao de rotacao simples a direita
+Noavl* rotacao_esq_dir2(Noavl* tree){
+    Noavl* aux = tree->esq; // aux recebe o filho a esquerda do no que vai ser rotacionado
+    Noavl* aux2 = aux->dir; // aux2 recebe o filho a direita do aux
+    tree->esq = aux2;       // o filho da esquerda do no que vai ser rotacionado recebe o filho da direita do aux
+    aux->dir = tree;        // o aux da direita recebe a raiz
+    tree = aux;             // a raiz recebe o aux
+
+    // recalcular o fator de balanceamento primeiro, recalcular o fator de balanceamento do filho a direita do no que vai ser rotacionado
+    update_fb(tree);
+    update_fb(aux);
+
+    return tree;
+}
+
+/* // exemplo de rotacao esquerda-direita
+Noavl* rotacao_esq_dir3(Noavl* candidato){
+    Noavl* filho = candidato->esq;
+    Noavl* neto = filho->dir;
+    candidato->esq = rotacao_simples_esq(candidato->esq);
+    Noavl* nova_raiz = rotacao_simples_dir(candidato);
+    
+    if (neto->fb == 1){
+        candidato->fb = -1;
+        filho->fb = neto->fb = 0;
+    }    
+    if (neto->fb == -1){
+        candidato->fb = neto->fb = 0;
+        filho->fb = 1;
+    }
+    if(neto->fb == 0)
+        candidato->fb =  filho->fb = 0;
+
+    return nova_raiz;
+} */
+
+
+// funcao para fazer a rotacao esquerda-direita
+Noavl* rotacao_dir_esq(Noavl* tree){
+    tree->dir = rotacao_simples_dir(tree->dir);
+    return rotacao_simples_esq(tree);
+}
+
+// funcao para fazer a rotacao direita-esquerda sem usar a funcao de rotacao simples a esquerda
+Noavl* rotacao_dir_esq2(Noavl* tree){
+    Noavl* aux = tree->dir; // aux recebe o filho a direita do no que vai ser rotacionado
+    Noavl* aux2 = aux->esq; // aux2 recebe o filho a esquerda do aux
+    tree->dir = aux2;       // o filho da direita do no que vai ser rotacionado recebe o filho da esquerda do aux
+    aux->esq = tree;        // o aux da esquerda recebe a raiz
+    tree = aux;             // a raiz recebe o aux
+
+    // recalcular o fator de balanceamento primeiro, recalcular o fator de balanceamento do filho a esquerda do no que vai ser rotacionado
+    update_fb(tree);
+    update_fb(aux);
+
+    return tree;
+}
 
 // funcao para fazer a rotacao dupla a direita
 Noavl* rotacao_dupla_dir(Noavl* tree){
